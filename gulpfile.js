5b681596a51cd9e5226443e1529d213723092ee7
Ewa <ewa_gonera@op.pl>
2016-04-07 22:47:30 +0200

    srera

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var jade = require('gulp-jade');
+
+
+gulp.task('templates', function() {
+  gulp.src('./src/jade/*.jade')
+    .pipe(jade({
+      pretty: true,
+      locals: {
+        title: 'Kalendarz imprez'
+      }
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('templates:watch', ['templates'], function () {
+  gulp.watch('./src/jade/**/*.jade', ['templates']);
+});
+
+gulp.task('js', function () {
+  return gulp.src('./src/js/**/*.js', {base: './src'})
+    .pipe(gulp.dest('./dist/'));
+});
+gulp.task('js:watch', ['js'], function () {
+  gulp.watch('./src/js/**/*.js', ['js']);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*')
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:watch', ['sass'], function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['js:watch', 'sass:watch', 'templates:watch']);
